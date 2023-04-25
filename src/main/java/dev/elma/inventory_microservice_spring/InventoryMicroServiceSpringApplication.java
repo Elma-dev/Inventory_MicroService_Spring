cd7191990b05c446577c617684b52b1436d24f88
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 22:40:48 +0100

    Initial commit

---
+++ src/main/java/dev/elma/inventory_microservice_spring/InventoryMicroServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.inventory_microservice_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMicroServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryMicroServiceSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elma/inventory_microservice_spring/InventoryMicroServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.inventory_microservice_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMicroServiceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
